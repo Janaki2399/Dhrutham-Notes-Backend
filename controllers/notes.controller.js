6925cd53edfc73ad057942356cf1d3f64b7043af
Janaki <janakishreeja23@gmail.com>
2021-06-24 10:55:06 +0400

    feat - add new note

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -1,1 +1,23 @@
-const getAllNotesOfUser = async (req, res) => {};
+const { Note } = require("../models/note.model");
+const { NotesList } = require("../models/notesList.model");
+export const addNote = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const body = req.body;
+    const note = new Note(body);
+    let noteList = await NotesList.findOne({ user: userId });
+    //   const newNoteList = "";
+    if (!noteList) {
+      noteList = new NotesList({ user: userId, list: [] });
+    }
+    await noteList.list.push(note.id);
+    await noteList.save();
+    res.status(200).json({ success: true, note: noteList });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      errorMessage: "Unable to add the note.Try again Later",
+    });
+  }
+};
+module.exports = { addNote };
--- routes/notes-router.js
+++ routes/notes-router.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const router = express.Router();
+const { addNote } = require("../controllers/notes.controller");
+router.post("/", addNote);
 module.exports = router;
