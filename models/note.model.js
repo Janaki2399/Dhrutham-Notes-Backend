60d87198b1cdcb5234e60db7be6519744aa3dbcf
Janaki <janakishreeja23@gmail.com>
2021-06-29 19:26:37 +0530

    feat - timestamp for note

--- models/note.model.js
+++ models/note.model.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: String,
+    isPinned: Boolean,
+    color: String,
+    labelList: [{ type: Schema.Types.ObjectId, ref: "Label" }],
   },
-  text: String,
-  isPinned: Boolean,
-  color: String,
-  labelList: [{ type: Schema.Types.ObjectId, ref: "Label" }],
-});
+  { timestamps: true }
+);
 const Note = mongoose.model("Note", noteSchema);
 
 module.exports = { Note };
